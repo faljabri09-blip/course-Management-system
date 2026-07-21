69bcb970a457622f218138a8d88e3720d2dc1647
Fatma <fatma@gmail.com>
2026-07-21 13:34:26 +0400

    adding constrain to models:

--- Course-Managment-System/Course-Managment-System/Models/Instructor.cs
+++ Course-Managment-System/Course-Managment-System/Models/Instructor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +10,24 @@ namespace Course_Managment_System.Models
 {
     public class Instructor
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int InstructorId { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string FullName { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public string Specialization { get; set; }
 
 
+        //relationship
+        //one to many
         public ICollection<Course> Courses { get; set; }
     }
 }
